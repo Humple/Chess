0b82b49cc84c1a0cd304ef175825c1604748b821
Humple <htr@outlook.com>
2012-11-16 13:41:10 +0300

    Network exception

---
+++ Chess/Network/NetworkException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chess.Network
+{
+    public class NetworkException: Exception
+    {
+        private String msg;
+        public String Message {
+            get {
+                return msg;
+            }
+        }
+        NetworkException()
+        {
+            msg = "Something wrong";
+        }
+        NetworkException(String message)
+        {
+            msg = message;
+        }
+    }
+}
